0b1de31757a7de5935eb7d5031738f4196bfa437
Kirill Malgichev <kmalgich@redhat.com>
2024-01-08 13:55:26 -0500

    Adding configuration options
    Migrating configurations to use environment variables and .env file
    Updating documentation

--- api/settings.py
+++ api/settings.py
@@ -1,17 +1,21 @@
-# TODO: Load settings from .env file or environment variables
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 # Number of workers
-max_workers: int = 2
+max_workers: int = int(os.getenv('MAX_WORKERS', 2))
 
 # Web server details
-host: str = '127.0.0.1'
-port: int = 8000
-log_level: str = 'info'
-reload: bool = False
+host: str = os.getenv('HOST', '127.0.0.1')
+port: int = int(os.getenv('PORT', 8000))
+log_level: str = os.getenv('LOG_LEVEL', 'info')
+reload: bool = bool(os.getenv('RELOAD', False))
 
-# Path to Ansible playbooks and Artifacts store
-# base_dir: str = os.path.dirname(os.path.abspath(__file__))
-base_dir: str = '/home/kmalgich/redhat/src/validation-scripts'
-scripts_path: str = 'runtime-automation'
-artifacts_path: str = 'artifacts'
-jobs_path: str = 'jobs'
+# Path to Ansible playbooks and Artifacts store)
+base_dir: str = os.getenv('BASE_DIR', '/app')
+scripts_path: str = os.getenv('SCRIPTS_PATH', 'runtime-automation')
+artifacts_path: str = os.getenv('ARTIFACTS_PATH', 'artifacts')
+jobs_path: str = os.getenv('JOBS_PATH', 'jobs')
